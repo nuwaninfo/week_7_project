89c6e5773603286e2972d22a017be79465471ed0
Nuwan Fernando <nuwaninfo@gmail.com>
2024-12-15 17:01:12 +0200

    change the local storage name

--- public/js/login.js
+++ public/js/login.js
@@ -27,7 +27,7 @@ const fetchData = async (event) => {
       const data = await response.json()
 
       if (data.token) {
-        localStorage.setItem("auth_token", data.token)
+        localStorage.setItem("token", data.token)
       }
     }
   } catch (error) {
